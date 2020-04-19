0b49351eda772f97f0e0d8ba3d6b68246f54da8d
mazari <luigi.mazari@gmail.com>
2020-04-19 18:28:56 +0200

    updated dpc data to 19/04

--- chartjs/js/worldmap.js
+++ chartjs/js/worldmap.js
@@ -1,31 +1,81 @@
-fetch('https://unpkg.com/world-atlas/countries-50m.json').then((r) => r.json()).then((data) => {
-      const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
 
-  const chart = new Chart(document.getElementById("canvas").getContext("2d"), {
-    type: 'choropleth',
-    data: {
-      labels: countries.map((d) => d.properties.name),
-      datasets: [{
-        label: 'Countries',
-        backgroundColor: (context) => {
-          if (context.dataIndex == null) {
-                return null;
-          }
-          const value = context.dataset.data[context.dataIndex];
-          return new Color('steelblue').lightness(value.value * 100).rgbString();
+var worldmap_config= {};
+var worldchart;
+
+fetch('https://unpkg.com/world-atlas/countries-50m.json').then((r) => r.json()).then((data) => {
+    const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
+    worldmap_config= {
+        type: 'choropleth',
+        data: {
+            labels: countries.map((d) => d.properties.name),
+            datasets: [{
+                label: 'Countries',
+                backgroundColor: (context) => {
+                    if (context.dataIndex == null) {
+                        return null;
+                    }
+                    const value = context.dataset.data[context.dataIndex];
+                    return new Color('steelblue').lightness(value.value * 100).rgbString();
+                },
+                data: countries.map((d) => ({feature: d, value: Math.random()})),
+            }]
         },
-        data: countries.map((d) => ({feature: d, value: Math.random()})),
-      }]
-    },
-    options: {
-      showOutline: true,
-      showGraticule: true,
-      legend: {
-        display: false
-      },
-      scale: {
-        projection: 'equalEarth'
-      }
-    }
-  });
+        options: {
+            showOutline: true,
+            showGraticule: true,
+            legend: {
+                display: false
+            },
+            scale: {
+                projection: 'equalEarth'
+            },
+            onClick: chartClickEvent
+        }
+    };
+
+    //create a drawing context on the canvas
+    var $element = document.getElementById("world_map");
+    var worldmap_ctx = $element.getContext("2d");
+
+    worldchart = new Chart(
+        worldmap_ctx,
+        worldmap_config
+    );
 });
+
+
+function chartClickEvent(event, array){
+    //https://stackoverflow.com/questions/46672925/chart-js-onclick-event-with-a-mixed-chart-which-chart-did-i-click
+    if(typeof newArr === 'undefined'){
+        newArr = array;
+    }
+
+    if (worldmap_config === 'undefined' || worldmap_config == null)
+    {
+      return;
+    }
+    if (event === 'undefined' || event == null)
+    {
+      return;
+    }
+    if (newArr === 'undefined' || newArr == null)
+    {
+      return;
+    }
+    if (newArr.length <= 0)
+    {
+      return;
+    }
+    var active = worldchart.getElementAtEvent(event);
+    console.log(active[0].feature.properties.name)
+}
+
+function removeCountryData(chart, country) {
+    //chart.data.labels.pop();
+    for(removalIndex=0; removalIndex<chart.data.datasets.length; removalIndex++){
+        if (chart.data.datasets[removalIndex].label == country){
+            chart.data.datasets.splice(removalIndex, 1);
+        }
+    }
+    chart.update();
+}
